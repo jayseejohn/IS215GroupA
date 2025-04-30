27d9e02c14d6ea11cd37d56d82e1855113c3b5d3
smdelfin-up <smdelfin@up.edu.ph>
2025-04-30 22:29:55 +0800

    Updated articles.php

--- articles.php
+++ articles.php
@@ -12,6 +12,21 @@
   </div>
 </section>
 
+<div class="row">
+  <div class="container">
+    <div class="alert alert-info">
+      <p class="m-0">
+        <h5><b>Your articles are <b>private</b> and saved only on your device.</b></h5>
+        <p class="m-0 alert alert-light text-justify">
+          Except for images stored on Amazon S3, 
+          the articles you see here — including pictures and texts — 
+          are saved in your browser and cannot be seen by anyone else. 
+          They will <b>automatically disappear one hour after they are uploaded</b>.
+        </p>
+      </p>
+    </div>
+  </div>
+</div>
 
 <div class="container my-5 p-0">
   <div class="row">
